0c3dd33ea42ddc5ae382df278634f2f7e9f3de92
Daniel Apt <info@iamapt.com>
2014-05-24 17:17:54 +0100

    Add stylesheet

--- index.php
+++ index.php
@@ -3,13 +3,9 @@ require_once('templates/template-files.php');
 
 get_header();?>
 
-<div class="jumbotron">
-	<h1>Discover how old you are</h1>
-	<p>We all perceive time in a different way. Lorem ipsum dolor sit amet conspecetur.</p>
-	<button class="btn btn-lg btn-primary">Find out</button>
+<div class="intro jumbotron">
+	<h1>Discover your age</h1>
+	<p>We all perceive time in a different way lorem ipsum conspecetur.</p>
 </div>
-
-
-
 
 <?php get_footer();
--- templates/template-files.php
+++ templates/template-files.php
@@ -11,11 +11,11 @@ function get_header($title){
 	<title>$title</title>
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css"/>
 	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="container">
 
 
 HERE;
@@ -24,7 +24,6 @@ HERE;
 function get_footer(){
 	echo <<<HERE
 
-	</div><!--end container-->
 </body>
 </html>
 
