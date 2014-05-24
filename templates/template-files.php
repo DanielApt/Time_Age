1ca43cafe768fcf662883004f80e12208aaf2167
Daniel Apt <info@iamapt.com>
2014-05-24 17:41:46 +0100

    Smoothly scroll to anchor tags

--- templates/template-files.php
+++ templates/template-files.php
@@ -14,6 +14,7 @@ function get_header($title){
 	<link rel="stylesheet" href="css/style.css"/>
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
 </head>
 <body>
 
