864a757ba8cb55acfdb9d37d50bd8e155be2196f
Daniel Apt <info@iamapt.com>
2014-05-24 17:40:28 +0100

    Vertically center text

--- index.php
+++ index.php
@@ -3,10 +3,20 @@ require_once('templates/template-files.php');
 
 get_header();?>
 
-<div class="intro jumbotron">
-	<h1>Discover your age</h1>
-	<p>We all perceive time in a different way lorem ipsum conspecetur.</p>
-	<p><a href=""><span class="glyphicon glyphicon-chevron-down"></span></a></p>
+<div class="intro jumbotron screen">
+	<div class="content">
+		<h1>Discover your age</h1>
+		<p>We all perceive time in a different way lorem ipsum conspecetur.</p>
+		<p><a href="#test"><span class="glyphicon glyphicon-chevron-down"></span></a></p>
+	</div>
+</div>
+
+<div id="test" class="test jumbotron screen">
+	<div class="content">
+		<p>Tap the button whenever you think 20 seconds has past.</p>
+		<p>Please don't use a phone, watch, or other time-telling devices.</p>
+		<p>Still 6 remaining.</p>
+	</div>
 </div>
 
 <?php get_footer();
