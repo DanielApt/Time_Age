2363072e6ee8cfe9fcda03dfb45e5bde83881708
Daniel Apt <info@iamapt.com>
2014-05-24 17:27:19 +0100

    Style intro page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ get_header();?>
 <div class="intro jumbotron">
 	<h1>Discover your age</h1>
 	<p>We all perceive time in a different way lorem ipsum conspecetur.</p>
+	<p><a href=""><span class="glyphicon glyphicon-chevron-down"></span></a></p>
 </div>
 
 <?php get_footer();
