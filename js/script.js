d98d94f09c314fe529d0dee5a8b3ff0f1c7e4d5c
Daniel Apt <info@iamapt.com>
2014-05-25 17:29:53 +0100

    Tiny indenting changes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 $(function() {
-  //initialise video
-  var BV = new $.BigVideo();
-  BV.init();
-  BV.show('http://vjs.zencdn.net/v/oceans.mp4', {ambient:true});
+    //initialise video
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('http://vjs.zencdn.net/v/oceans.mp4', {
+        ambient: true
+    });
 
-    $('#instructions').waypoint(function() {
+    $('#instructions').waypoint(function(direction) {
         $(this).find('.content p').each(function(i) {
             $(this).delay(i * 3000).slideDown('slow');
         });
     });
 
-  //smoothly scroll to tags
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    //smoothly scroll to tags
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
