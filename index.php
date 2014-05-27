441841fb8d7e73b2484ee54c4c89a3ef46e4fee5
Daniel Apt <info@iamapt.com>
2014-05-27 14:55:42 +0100

    Change some of the writing on the website

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ get_header();?>
 <div class="intro jumbotron screen">
 	<div class="content">
 		<h1>Discover your age</h1>
-		<p>We all perceive time in a different way lorem ipsum conspecetur.</p>
+		<p>We all perceive time in a different way, discover your <em>Time Perception Age</em>.</p>
 		<a class="btn btn-lg btn-line btn-round" href="#instructions"><span class="glyphicon glyphicon-chevron-down"></span></a>
 	</div>
 </div>
@@ -16,7 +16,6 @@ get_header();?>
 		<p>You will be asked to press a button <em>every 20 seconds</em>.</p>
 		<p>In total you will pres this button <em>6 times</em>.</p>
 		<p>Use your intuition, not a clock, watch or phone.</p>
-		<p>Good luck</p>
 		<p><a href="#test" id="start-test" class="btn btn-lg btn-line">Start test</a></p>
 	</div>
 </div>
